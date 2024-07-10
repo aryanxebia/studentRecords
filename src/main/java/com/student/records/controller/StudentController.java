e049f38c66c6e2f4b9ed8d6f1021ffdcd50b2a6f
Aryan Saini <aryan.saini@xebia.com>
2024-07-10 13:45:30 +0530

    get student records api added

--- src/main/java/com/student/records/controller/StudentController.java
+++ src/main/java/com/student/records/controller/StudentController.java
@@ -1,15 +1,18 @@
 package com.student.records.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.student.records.entity.Student;
 import com.student.records.exception.StudentRecordsException;
 import com.student.records.service.StudentService;
 
@@ -27,5 +30,12 @@ public class StudentController {
 
         studentService.saveStudentsFromCsv(file);
         return new ResponseEntity<>("File uploaded and data saved to database!", HttpStatus.OK);
+    }
+    
+    
+    @GetMapping
+    public ResponseEntity<List<Student>> getAllStudents() {
+        List<Student> students = studentService.getAllStudents();
+        return new ResponseEntity<>(students, HttpStatus.OK);
     }
 }
--- src/main/java/com/student/records/service/StudentService.java
+++ src/main/java/com/student/records/service/StudentService.java
@@ -1,11 +1,14 @@
 package com.student.records.service;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import com.student.records.entity.Student;
 import com.student.records.exception.StudentRecordsException;
 
 public interface StudentService {
     void saveStudentsFromCsv(MultipartFile file) throws StudentRecordsException, IOException;
+    List<Student> getAllStudents();
 }
