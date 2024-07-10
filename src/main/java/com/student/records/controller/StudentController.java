88433647a10b83e4fe9ab03da8ed48d754985bba
Aryan Saini <aryan.saini@xebia.com>
2024-07-10 22:16:07 +0530

    Minor refactoring

--- src/main/java/com/student/records/controller/StudentController.java
+++ src/main/java/com/student/records/controller/StudentController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,21 +16,25 @@ import org.springframework.web.multipart.MultipartFile;
 import com.student.records.entity.Student;
 import com.student.records.exception.StudentRecordsException;
 import com.student.records.service.StudentService;
+import com.student.records.util.ConstantUtil;
 
+
+
+@RequestMapping("/api/v1/")
 @RestController
 public class StudentController {
 
     @Autowired
     private StudentService studentService;
 
-    @PostMapping("/upload")
+    @PostMapping("upload")
     public ResponseEntity<String> uploadCsv(@RequestParam("file") MultipartFile file) throws StudentRecordsException, IOException {
         if (file.isEmpty()) {
         	 throw new StudentRecordsException(HttpStatus.BAD_REQUEST, com.student.records.util.ConstantUtil.CSV_NOT_VALID);	
         }
 
         studentService.saveStudentsFromCsv(file);
-        return new ResponseEntity<>("File uploaded and data saved to database!", HttpStatus.OK);
+        return new ResponseEntity<>(ConstantUtil.FILE_UPLOAD_SUCCESSFUL, HttpStatus.OK);
     }
     
     
--- src/main/java/com/student/records/util/ConstantUtil.java
+++ src/main/java/com/student/records/util/ConstantUtil.java
@@ -3,4 +3,5 @@ package com.student.records.util;
 public class ConstantUtil {
 	public static final String CSV_NOT_VALID = "This is not a valid CSV file";
 	public static final String INVALID_STUDENT_ID="Student ID must be provided in the CSV file";
+	public static final String FILE_UPLOAD_SUCCESSFUL = "File uploaded and data saved to database!";
 }
